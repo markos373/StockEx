919cd7ed58232e6bc246f11dc6edae4740627b0b
Christopher Turgeon <46657395+christurgeon@users.noreply.github.com>
2019-08-16 15:21:43 -0400

    Create run.cmd

---
+++ StockEx/src/run.cmd
@@ -0,0 +1,2 @@
+javac -cp "../org.json.jar;./graphs/jcommon-1.0.23.jar;./graphs/jfreechart-1.0.19.jar;./" *.java
+java -cp "../org.json.jar;./graphs/jcommon-1.0.23.jar;./graphs/jfreechart-1.0.19.jar;./;../mysql-connector-java-8.0.17.jar" UserView
